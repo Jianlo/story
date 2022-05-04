01d1138f0eac5e8d773566db3a7a0d47c0b43bc9
Jianlo <2934316811@qq.com>
2022-05-04 21:59:01 +0800

    为用户创建session和cookie

--- src/main/java/com/jian/controller/UserController.java
+++ src/main/java/com/jian/controller/UserController.java
@@ -4,55 +4,93 @@ import com.jian.base.ResultData;
 import com.jian.base.StatusCode;
 import com.jian.dto.ProfileDto;
 import com.jian.dto.SignUpDto;
-import com.jian.dto.SignInDto;
 import com.jian.entity.User;
 import com.jian.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 
 @Controller
 public class UserController {
 
+    private static final Logger logger = LogManager.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     //登录界面
     @RequestMapping("/")
-    public String login(Model model){
-        model.addAttribute("signInDto",new SignInDto());
+    public String login(){
         return "signIn";
     }
 
     //注册界面
     @RequestMapping("/register")
-    public String register(Model model){
-        model.addAttribute("signUpDto",new SignUpDto());
+    public String register(){
         return "signUp";
     }
 
 
     //用户登录
     @PostMapping("/user/signIn")
-    public String signIn(@ModelAttribute SignInDto signInDto){
+    public String signIn(@RequestParam String username, @RequestParam String password, String rememberMe, HttpServletRequest request, HttpServletResponse response){
 
-        User user = userService.findByUsername(signInDto.getUsername());
+        User user = userService.findByUsername(username);
         if (user != null){
-            if (user.getPassword().equals(signInDto.getPassword())){
-                System.out.println("登录成功");
+            if (user.getPassword().equals(password)){
+
+                logger.info("登录成功");
+
+                //把用户信息存储在Session中
+                HttpSession session = request.getSession();
+                session.setAttribute("user",user);
+
+                //如果勾选了记住密码
+                if (rememberMe !=null){
+                    Cookie cookie1 = new Cookie("username",user.getUsername());
+                    Cookie cookie2 = new Cookie("password",user.getPassword());
+
+                    cookie1.setPath("/");
+                    cookie2.setPath("/");
+
+                    cookie1.setMaxAge(3*24*60*60);
+                    cookie2.setMaxAge(3*24*60*60);
+
+                    response.addCookie(cookie1);
+                    response.addCookie(cookie2);
+
+                }
+                //未勾选记住密码则删除用户的cookie
+                else {
+                    Cookie[] cookies = request.getCookies();
+                    if (cookies != null){
+                        for (Cookie cookie:cookies) {
+                            if (cookie.getName().equals("username") || cookie.getName().equals("password")){
+                                cookie.setMaxAge(0);
+                                cookie.setPath("/");
+                                response.addCookie(cookie);
+                            }
+                        }
+                    }
+                }
                 return "index";
             }
             else {
-                System.out.println("密码错误");
+                logger.info("密码错误");
                 return "signIn";
             }
         }
         else {
-            System.out.println("用户名不存在");
+            logger.info("用户名不存在");
             return "signIn";
         }
 
@@ -60,11 +98,11 @@ public class UserController {
 
     //用户注册
     @PostMapping("/user/signUp")
-    public String signUp(@ModelAttribute SignUpDto signUpDto){
+    public String signUp(SignUpDto signUpDto){
 
         //判断该用户名是否已被注册
         if (userService.findByUsername(signUpDto.getUsername()) != null){
-            System.out.println("该用户名已被注册");
+            logger.info("该用户名已被注册");
             return "signUp";
         }
         else {
@@ -84,7 +122,7 @@ public class UserController {
             user.setPhotoUrl(defaultPhotoUrl);
 
             userService.addUser(user);
-            System.out.println("注册成功");
+            logger.info("注册成功");
             return "signIn";
         }
 
